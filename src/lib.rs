f5d28fd0343874762d827356009a654691a47323
Andy Bell <andy.bell@displaynote.com>
2020-07-14 18:32:19 +0200

    Add soem basic calls to test out the crate rust-cpp

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,65 @@
 use cpp::cpp;
+use cpp::cpp_class;
 
 cpp!{{
     #include "src/cpp/agorasdk/AgoraSdk.h"
+    using std::string;
 }}
 
-struct Recorder {
-    id: String
+cpp_class!(pub unsafe struct Config as "agora::recording::RecordingConfig");
+impl Config {
+    fn new() -> Self {
+        unsafe { cpp!([] -> Config as "agora::recording::RecordingConfig" {return agora::recording::RecordingConfig();}) }
+    }
 }
 
+cpp_class!(pub unsafe struct Recorder as "agora::AgoraSdk");
 impl Recorder {
-    pub fn new() -> Recorder {
+    fn new() -> Self {
+        unsafe { cpp!([] -> Recorder as "agora::AgoraSdk" {return agora::AgoraSdk();}) }
+    }
+
+    fn create_channel(&self, app_id: &str, channel_key: &str, name: &str, uid: u32, config: &Config) -> bool {
         
-        let r = unsafe {
-            cpp!([] {
-                agora::AgoraSdk recorder;
-            })
-        };
+        let app_id = app_id.as_ptr();
+        let name = name.as_ptr();
+        let channel_key = channel_key.as_ptr();
         
-        Recorder {
-            id: String::from("test")
-        }   
+        unsafe {
+            cpp!([  self as "agora::AgoraSdk*", 
+                    app_id as "const char *",
+                    channel_key as "const char *",
+                    name as "const char *",
+                    uid as "int",
+                    config as "agora::recording::RecordingConfig*"
+                    ] -> bool as "bool" {
+                        return self->createChannel(app_id, channel_key, name, uid, *config);
+                    }
+
+            )
+        }
+    }
+    
+    fn update_mix_mode_setting(&self, width: u32, height: u32, is_video_mix: bool) {
+        unsafe {
+            cpp!([self as "agora::AgoraSdk*",
+                width as "int",
+                height as "int", 
+                is_video_mix as "bool"] {
+                    self->updateMixModeSetting(width, height, is_video_mix);
+                }
+
+            )
+        }
+    }
+
+    fn leave_channel(&self) -> bool {
+        unsafe {
+            cpp!([self as "agora::AgoraSdk*"] -> bool as "bool" {
+                    return self->leaveChannel();
+                }
+            )
+        }
     }
 }
 
@@ -30,6 +70,6 @@ mod tests {
     #[test]
     fn create() {
         let recorder = Recorder::new();
-        assert_eq!(0, 1);
+
     }
 }
