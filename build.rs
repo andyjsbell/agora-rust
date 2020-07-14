1f56912094c72370871a5ffcedda185f57c72e92
Andy Bell <andy.bell@displaynote.com>
2020-07-14 14:12:52 +0200

    Build in agorasdk

--- build.rs
+++ build.rs
@@ -1,8 +1,13 @@
 extern crate cpp_build;
 
 fn main() {
+
     cpp_build::Config::new()
+        .file("src/cpp/agorasdk/AgoraSdk.cpp")
         .include("src/cpp/include")
         .include("src/cpp")
         .build("src/lib.rs");
+
+    println!("cargo:rustc-link-search={}", "src/cpp/libs");
+    println!("cargo:rustc-link-lib=recorder");
 }
