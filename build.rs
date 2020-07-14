150d13dfd68522e33f5da31ffff3fc69d8c1c7db
Andy Bell <andy.bell@displaynote.com>
2020-07-14 13:49:44 +0200

    Include directories

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 extern crate cpp_build;
 
 fn main() {
-    cpp_build::build("src/lib.rs");
+    cpp_build::Config::new()
+        .include("src/cpp/include")
+        .include("src/cpp")
+        .build("src/lib.rs");
 }
