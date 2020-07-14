e955269163de132975c83f347ecce692945a1f40
Andy Bell <andy.bell@displaynote.com>
2020-07-14 13:50:19 +0200

    Some basic code to create the principal class

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,35 @@
+use cpp::cpp;
+
+cpp!{{
+    #include "src/cpp/agorasdk/AgoraSdk.h"
+}}
+
+struct Recorder {
+    id: String
+}
+
+impl Recorder {
+    pub fn new() -> Recorder {
+        
+        let r = unsafe {
+            cpp!([] {
+                agora::AgoraSdk recorder;
+            })
+        };
+        
+        Recorder {
+            id: String::from("test")
+        }   
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn create() {
+        let recorder = Recorder::new();
+        assert_eq!(0, 1);
     }
 }
