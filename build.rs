e7df0883281531d317fa5be687b3de4f652fc628
Andy Bell <andy.bell@displaynote.com>
2020-07-15 19:44:37 +0200

    Set search paths

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
 extern crate cpp_build;
+use std::env;
+use std::path::Path;
 
 fn main() {
+    
+    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     cpp_build::Config::new()
         .file("src/cpp/agorasdk/AgoraSdk.cpp")
@@ -8,6 +12,6 @@ fn main() {
         .include("src/cpp")
         .build("src/lib.rs");
 
-    println!("cargo:rustc-link-search={}", "src/cpp/libs");
+    println!("cargo:rustc-link-search={}", Path::new(&dir).join("src/cpp/libs").display());
     println!("cargo:rustc-link-lib=recorder");
 }
