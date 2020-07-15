3ccbcec8e92efdf259ff716f4db8b11d51930e23
Andy Bell <andy.bell@displaynote.com>
2020-07-15 15:52:22 +0200

    set recording path

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use cpp::cpp;
 use cpp::cpp_class;
+use std::ffi::{CString, CStr};
+use std::os::raw::c_char;
 
 cpp!{{
     #include "src/cpp/agorasdk/AgoraSdk.h"
@@ -29,8 +31,27 @@ impl Config {
             })
         }    
     }
-}
 
+    fn set_recording_path(&self, path: &str) {
+        let path = CString::new(path).unwrap().into_raw();
+        unsafe {
+            cpp!([  self as "agora::recording::RecordingConfig*",
+                    path as "const char *"] {
+                self->appliteDir = path;
+            })
+        }
+    }
+
+    fn recording_path(&self) -> Result<&str, std::str::Utf8Error> {
+        let p = unsafe {
+            cpp!([self as "agora::recording::RecordingConfig*"] -> *const c_char as "const char *" {
+                return self->appliteDir;
+            })
+        } as *const i8;
+        let c = unsafe {CStr::from_ptr(p)};
+        c.to_str()
+    }
+}
 
 cpp_class!(pub unsafe struct Layout as "agora::linuxsdk::VideoMixingLayout");
 impl Layout {
@@ -117,5 +138,16 @@ mod tests {
         assert!(!config.is_mixing_enabled(), "should be false from the start");
         config.set_mixing_enabled(true);
         assert!(config.is_mixing_enabled(), "should be true after updating");
+    }
+
+    #[test]
+    fn config_set_recording_path() {
+        let config = Config::new();
+        let str = "test";
+        config.set_recording_path(str);
+        assert!(config.recording_path().is_ok());
+        let path = config.recording_path().unwrap();
+
+        assert!(path == str);
     }
 }
