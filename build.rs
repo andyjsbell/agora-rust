eeff72dd76280cf38d9d732dc57562d1b26af073
Andy Bell <andy.bell@displaynote.com>
2020-09-15 05:01:00 +0200

    Copy service to /tmp

--- build.rs
+++ build.rs
@@ -1,10 +1,25 @@
 extern crate cpp_build;
 use std::env;
 use std::path::Path;
+use std::process::Command;
 
 fn main() {
-    
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let mut extract = Command::new("tar");
+    extract
+        .arg("xvfz")
+        .arg("Agora_Recording_SDK_for_Linux_v3.0.1.tar");
+    extract.status().expect("failed to extract SDK");
+
+    let mut cp = Command::new("cp");
+    cp.arg("./Agora_Recording_SDK_for_Linux_FULL/bin/AgoraCoreService")
+        .arg("/tmp/AgoraCoreService");
+    cp.status().expect("failed to copy service");
+
+    let mut rm = Command::new("rm");
+    rm.arg("-rf").arg("./Agora_Recording_SDK_for_Linux_FULL");
+    rm.status().expect("failed to clean up");
 
     cpp_build::Config::new()
         .file("src/cpp/agorasdk/AgoraSdk.cpp")
@@ -12,6 +27,9 @@ fn main() {
         .include("src/cpp")
         .build("src/lib.rs");
 
-    println!("cargo:rustc-link-search={}", Path::new(&dir).join("src/cpp/libs").display());
+    println!(
+        "cargo:rustc-link-search={}",
+        Path::new(&dir).join("src/cpp/libs").display()
+    );
     println!("cargo:rustc-link-lib=recorder");
 }
