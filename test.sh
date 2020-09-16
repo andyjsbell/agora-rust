273ef963f9715b0d322a8d4bbe09f2816e7bb784
Andy Bell <andy.bell@displaynote.com>
2020-09-16 21:40:42 +0200

    set to /tmp

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export AGORA_CORE_PATH=/home/andy/devel
+export AGORA_CORE_PATH=/tmp
 export CHANNEL=demo
 export APP_ID=f4e3892ffa7c46dc819105abc11f6805
 export TOKEN=0066ab36d096e7441d5b31e2dca9570b4a2IAAncMfnJ5HHwSQb7rVkpB1k+ObRSlpQTOfb6VEb55rKh6DfQtYAAAAAEAArla295KhgXwEAAQDlqGBf
